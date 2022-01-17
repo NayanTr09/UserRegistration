cb7f367674cd3be9f95e7cb1d1ad55db35261dbe
NayanTr09 <nayantripathi42@gmail.com>
2022-01-17 18:24:00 +0530

    Welcome Message

---
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.userregistration;
+
+public class UserRegistration {
+    public static void welcome() {
+        System.out.println("Welcome to User Registration System Problem");
+    }
+
+    public static void main(String[] args) {
+        welcome();
+    }
+}
