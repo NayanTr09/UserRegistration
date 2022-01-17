86b3044662b800c0ce78870f94decc572f08b2cb
NayanTr09 <nayantripathi42@gmail.com>
2022-01-17 18:35:02 +0530

    Added : Checking First Name Functionality

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,11 +1,22 @@
 package com.userregistration;
 
-public class UserRegistration {
-    public static void welcome() {
-        System.out.println("Welcome to User Registration System Problem");
-    }
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public static void main(String[] args) {
-        welcome();
+public class UserRegistration {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void validFirstName(){
+        System.out.print("Enter the First Name : ");
+        String name = scanner.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean r = m.matches();
+        if (r)
+            System.out.println("First name is valid");
+        else
+            System.out.println("First name is !Invalid");
     }
 }
